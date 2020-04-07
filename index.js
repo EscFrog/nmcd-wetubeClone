a49b869fd79152f93f3ebb7cd403ab9fb186bd94
walterpark <neodust223@gmail.com>
2020-04-08 00:02:00 +0900

    complete #2.4

--- index.js
+++ index.js
@@ -7,4 +7,17 @@ function handleListening() {
 	console.log(`Listening on: http://localhost:${PORT}`);
 }
 
+// express().get 으로 실행한 함수는 request, response 오브젝트를 argument로 받는다.
+function handleHome(req, res) {
+	res.send("Hello from home");
+}
+
+function handleProfile(req, res) {
+	res.send("You are on my profile");
+}
+
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
+
 app.listen(PORT, handleListening);
