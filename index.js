c41c9618879b02c73f26704a44a2f869fbce80c1
walterpark <neodust223@gmail.com>
2020-04-09 00:58:47 +0900

    complete #2.5
    babel을 설치해서 적용하고
    index.js의 몇몇 함수를 ES6스타일로 변경하고
    devDependencies로 nodemon(저장할 때마다 서버를 새로 띄워주는 도구)를 설치함

--- index.js
+++ index.js
@@ -1,20 +1,35 @@
-const express = require("express");
+import express from "express";
+// 위 코드는 ES6에서만 지원. babel에 의해 해석되면 아래 코드와 같음
+// const express = require("express");
 const app = express();
 
 const PORT = 4000;
 
+const handleListening = () =>
+	console.log(`Listening on: http://localhost:${PORT}`);
+// 위 코드는 ES6에서만 지원. arrow function 이라고 함. babel에 의해 해석되면 아래 코드와 같음
+/*
 function handleListening() {
 	console.log(`Listening on: http://localhost:${PORT}`);
 }
+*/
 
+const handleHome = (req, res) => res.send("Hello from home");
+// 위 코드는 ES6에서만 지원. arrow function 이라고 함. babel에 의해 해석되면 아래 코드와 같음
 // express().get 으로 실행한 함수는 request, response 오브젝트를 argument로 받는다.
+/*
 function handleHome(req, res) {
 	res.send("Hello from home");
 }
+*/
 
+const handleProfile = (req, res) => res.send("You are on my profile");
+// 위 코드는 ES6에서만 지원. arrow function 이라고 함. babel에 의해 해석되면 아래 코드와 같음
+/*
 function handleProfile(req, res) {
 	res.send("You are on my profile");
 }
+*/
 
 app.get("/", handleHome);
 
