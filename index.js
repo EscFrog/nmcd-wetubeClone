ab772d0a13da93b204cf31c0e74e020ffceff4d7
walterpark <neodust223@gmail.com>
2020-04-09 12:35:48 +0900

    completed #2.6 #2.7
    nodemon에 1초 딜레이 추가 / morgan, helmet, cookie-parser, body-parser 라는 미들웨어 추가.

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 import express from "express";
 // 위 코드는 ES6에서만 지원. babel에 의해 해석되면 아래 코드와 같음
 // const express = require("express");
+import morgan from "morgan";
+import helmet from "helmet";
+import cookiePaser from "cookie-parser";
+import bodyparser from "body-parser";
+
 const app = express();
 
 const PORT = 4000;
@@ -30,6 +35,24 @@ function handleProfile(req, res) {
 	res.send("You are on my profile");
 }
 */
+
+/* 미들웨어 예제
+const betweenHome = (req, res, next) => {
+	console.log("I'm in between");
+	next();
+};
+
+// 미들웨어 추가. 몇 개든 추가할 수 있지만 순서가 중요함. 아래에 있는 get함수로 가는 Route에서만 미들웨어가 실행됨
+app.use(betweenHome);
+
+// app.get("/", betweenHome, handleHome);
+*/
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan("dev"));
 
 app.get("/", handleHome);
 
