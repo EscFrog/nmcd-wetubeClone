796179d10c420719aba12b1e32abdafc93a4cabc
walterpark <neodust223@gmail.com>
2020-04-14 22:42:02 +0900

    prepare #2.15
    main.pug 레이아웃을 모든 .pug 파일에 적용함.

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,5 +4,5 @@ export const logout = (req, res) => res.render("logout");
 
 export const users = (req, res) => res.render("users");
 export const userDetail = (req, res) => res.render("userDetail");
-export const editProfile = (req, res) => res.render("dditProfile");
+export const editProfile = (req, res) => res.render("editProfile");
 export const changePassword = (req, res) => res.render("changePassword");
